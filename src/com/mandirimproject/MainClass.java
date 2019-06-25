c0cdad4b88db49900d4884813a5ae07aac03566e
Elvis Tanaem <elvis.june@wgs.co.id>
2019-06-25 08:33:45 +0700

    Fibonacci commit

---
+++ src/com/mandirimproject/MainClass.java
@@ -0,0 +1,29 @@
+package com.mandirimproject;
+
+import java.util.Scanner;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+	      Scanner scanner = new Scanner( System.in );
+	      int a, b, n, hasil;
+	      System.out.print( "Input : " );
+	      a = scanner.nextInt();
+	      b = scanner.nextInt();
+	      n = scanner.nextInt();
+	      
+	      System.out.print("Output : ");
+	      System.out.print(a + ", " + b);    
+		  
+			 for(int i = 2; i < n; ++i)    
+			 {    
+			  hasil = a + b;  
+			  System.out.print(", " + hasil);    
+			  a = b;    
+			  b = hasil;    
+			 }
+
+	}
+
+}
